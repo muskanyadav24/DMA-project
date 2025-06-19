6f3594d0d17f02c818018929971679b2a016b370
Muskan yadav <muskanyadav92163@gmail.com>
2025-06-19 09:22:19 +0530

    Task-2-complete

--- dma-2-array.cpp
+++ dma-2-array.cpp
@@ -1,1 +1,25 @@
+#include<iostream>
+using namespace std;
+int main(){
 
+  int *intArray = new int [3] {1,2,3};
+  string *strArray = new string [3] {"Kru","Keyu","Anju"};
+
+  cout << "Integer array :- ";
+    
+    for(int i=0;i<3;i++){
+      cout << intArray[i] << " , ";
+    }
+    cout << endl;
+  
+    cout << "String array :- ";
+
+    for(int j=0;j<3;j++){
+      cout << strArray[j] << " , ";
+    }
+
+    delete[] intArray;
+    delete[] strArray;
+  
+  return 0;
+}
